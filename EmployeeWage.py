39f28187076710371a80d960132e6ea3f2813fea
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-11-30 13:52:03 +0530

    Employee Wage Computation

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,14 +1,1 @@
-import random
-
-class EmployeeWage:
-    def checkAttendance(self):
-        attendance = (random.randint(0, 1))
-        if attendance == 1:
-            return 'Employee is present'
-        else:
-            return 'Employee is absent'
-
-if __name__ == '__main__':
-    print('Welcome to Employee Wage Computation Program')
-    emp = EmployeeWage()
-    print(emp.checkAttendance())
+print("Welcome to Employee Wage Computation Program")
