879c52548767d541a0dcfec05df7315774d67493
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-11-30 16:09:58 +0530

    Calculate part time wage using switch case

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -4,25 +4,28 @@ class EmployeeWage:
     wage_Per_Hour = 20
     workHour = 0
     dailyWage = 0
+    isAbsent = 0
     isFullTime = 1
     isPartTime = 2
     fullTimeHours = 8
     partTimeHours = 4
+
+
     def checkAttendance(self):
-        attendance = (random.randint(0, 2))
-        if attendance == EmployeeWage.isFullTime:
-            EmployeeWage.workHour = EmployeeWage.fullTimeHours
-            return 'Employee is present full time'
+        attendance = random.randint(0, 2)
+        return attendance
 
-        elif attendance == EmployeeWage.isPartTime:
-            EmployeeWage.workHour == EmployeeWage.partTimeHours
-            return 'Employee is present part time'
+    def perDayHours(self, attendance_of_emp):
+        switcher = {
+            0: 0,
+            1: EmployeeWage.fullTimeHours,
+            2: EmployeeWage.partTimeHours
+        }
+        EmployeeWage.workHour = switcher.get(attendance_of_emp)
+        print(EmployeeWage.workHour)
 
-        else:
-            EmployeeWage.workHour = 0
-            return 'Employee is absent'
     def calculateWage(self):
-        EmployeeWage.dailyWage = EmployeeWage.workHour * EmployeeWage.wage_Per_Hour
+        EmployeeWage.dailyWage =  EmployeeWage.wage_Per_Hour * EmployeeWage.workHour
         print('Daily Wage is')
         return EmployeeWage.dailyWage
 
@@ -30,4 +33,6 @@ if __name__ == '__main__':
     print('Welcome to Employee Wage Computation Program')
     emp = EmployeeWage()
     print(emp.checkAttendance())
+    print(emp.perDayHours(emp.checkAttendance()))
+
     print(emp.calculateWage())
