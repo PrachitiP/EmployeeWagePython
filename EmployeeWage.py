8ae4ccfe486ace2fec209a1eaeb1b4dc8e345efa
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-12-01 20:25:07 +0530

    Refactored in Function

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,5 +1,4 @@
 import random
-
 class EmployeeWage:
     wage_Per_Hour = 20
     workHour = 0
@@ -9,30 +8,45 @@ class EmployeeWage:
     isPartTime = 2
     fullTimeHours = 8
     partTimeHours = 4
+    empDailyWage = 0
 
+    def checkAttendance(cls):
+        presence = random.randint(0, 2)
+        return presence
 
-    def checkAttendance(self):
-        attendance = random.randint(0, 2)
-        return attendance
+    def employee_work_hours(cls, attendance_of_employee):
+        if attendance_of_employee == EmployeeWage.isFullTime:
+            print("Employee is present for : Full Time")
+            return EmployeeWage.fullTimeHours
+        elif attendance_of_employee == EmployeeWage.isPartTime:
+            print("Employee is present for : Part time")
+            return EmployeeWage.partTimeHours
+        else:
+            print("Employee is Absent")
+            return 0
 
-    def perDayHours(self, attendance_of_emp):
-        switcher = {
-            0: 0,
-            1: EmployeeWage.fullTimeHours,
-            2: EmployeeWage.partTimeHours
-        }
-        EmployeeWage.workHour = switcher.get(attendance_of_emp)
-        print(EmployeeWage.workHour)
+    def calculateMonthlyWage(self):
+        monthlyWage = 0
+        empHours = 0
+        day = 1
+        attendance = 0
+        while True:
+            print(f"day {day} : ")
+            attendance = EmployeeWage.checkAttendance(self)
+            EmployeeWage.workHours = EmployeeWage.employee_work_hours(self, attendance)
+            empHours = empHours + EmployeeWage.workHour
+            EmployeeWage.employee_daily_wage = EmployeeWage.wage_Per_Hour * EmployeeWage.workHour
+            print(f"Employee's salary for day {day} is : {EmployeeWage.empDailyWage}")
+            monthlyWage = monthlyWage + EmployeeWage.empDailyWage
+            day = day + 1
+            if (empHours >= 100) or (day >= 20):
+                print(f"Employee hours : {empHours} and Days : {day}")
+                break
 
-    def calculateWage(self):
-        EmployeeWage.dailyWage =  EmployeeWage.wage_Per_Hour * EmployeeWage.workHour
-        print('Daily Wage is')
-        return EmployeeWage.dailyWage
+        print(f"\nEmployee's Salary for the Entire Month is: {monthlyWage}")
+
 
 if __name__ == '__main__':
     print('Welcome to Employee Wage Computation Program')
     emp = EmployeeWage()
-    print(emp.checkAttendance())
-    print(emp.perDayHours(emp.checkAttendance()))
-
-    print(emp.calculateWage())
+    print(emp.calculateMonthlyWage())
